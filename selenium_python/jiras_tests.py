88c29c0d39c3545af7866d7c56f99d655a91cf13
Łukasz Jabłoński <l.jablonski@ks-software.com>
2015-12-02 15:24:25 +0100

    jiras tests files renamed

--- selenium_python/jiras_tests.py
+++ selenium_python/jiras_tests.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 import unittest, time, re
-from taa_vm.jiras.connect_409 import Connect409Test
-from taa_vm.jiras.connect_415 import Connect415Test
-from taa_vm.jiras.connect_460 import Connect460Test
-from taa_vm.jiras.connect_462 import Connect462Test
-from taa_vm.jiras.connect_465 import Connect465Test
-from taa_vm.jiras.connect_534 import Connect534Test
-from taa_vm.jiras.connect_589 import Connect589Test
-from taa_vm.jiras.connect_720 import Connect720Test
-from taa_vm.jiras.connect_794 import Connect794Test
-from taa_vm.jiras.connect_886 import Connect886Test
-from taa_vm.jiras.connect_939 import Connect939Test
-from taa_vm.jiras.connect_951 import Connect951Test
-from taa_vm.jiras.connect_1038 import Connect1038Test
-from taa_vm.jiras.connect_1152 import Connect11522Test
-from taa_vm.jiras.connect_1503 import Connect1503Test
+from taa_vm.jiras.connect_409_test import Connect409Test
+from taa_vm.jiras.connect_415_test import Connect415Test
+from taa_vm.jiras.connect_460_test import Connect460Test
+from taa_vm.jiras.connect_462_test import Connect462Test
+from taa_vm.jiras.connect_465_test import Connect465Test
+from taa_vm.jiras.connect_534_test import Connect534Test
+from taa_vm.jiras.connect_589_test import Connect589Test
+from taa_vm.jiras.connect_720_test import Connect720Test
+from taa_vm.jiras.connect_794_test import Connect794Test
+from taa_vm.jiras.connect_886_test import Connect886Test
+from taa_vm.jiras.connect_939_test import Connect939Test
+from taa_vm.jiras.connect_951_test import Connect951Test
+from taa_vm.jiras.connect_1038_test import Connect1038Test
+from taa_vm.jiras.connect_1152_test import Connect11522Test
+from taa_vm.jiras.connect_1503_test import Connect1503Test
 
 
 if __name__ == "__main__":
